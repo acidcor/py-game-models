e93147a8c71afa2a7ff1eac96af8659116084e61
acidcor <deadfoxua1337@gmail.com>
2025-11-01 17:57:00 +0100

    flake8fix

--- db/models.py
+++ db/models.py
@@ -9,7 +9,11 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(unique=True, max_length=255)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skill")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skill"
+    )
 
 
 class Guild(models.Model):
@@ -21,6 +25,15 @@ class Player(models.Model):
     nickname = models.CharField(unique=True, max_length=255)
     email = models.EmailField(max_length=255, unique=False)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE,  related_name="player")
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, related_name="player")
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="player"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="player"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
